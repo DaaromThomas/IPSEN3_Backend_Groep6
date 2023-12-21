2f7cb16a361917591db63fcd19835192a6f19462
Ruben.Colijn <s1142666@student.hsleiden.nl>
2023-12-21 11:04:44 +0100

    Fix: Emailcontroller has correct name and enpoint is named better

--- src/main/java/com/hsleiden/vdlelie/controllers/EmailController.java
+++ src/main/java/com/hsleiden/vdlelie/controllers/EmailController.java
@@ -1,25 +1,25 @@
 package com.hsleiden.vdlelie.controllers;
 
 import com.hsleiden.vdlelie.services.EmailService;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EmaiController {
+public class EmailController {
 
     private final EmailService emailService;
 
-    public EmaiController(EmailService emailService){
+    public EmailController(EmailService emailService){
         this.emailService = emailService;
     }
 
 
-    @PostMapping("/email")
-//    @PreAuthorize("HasAnyRole('USER', 'ADMIN')")
+    @PostMapping("/email/lowonstock")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
     public void email(@RequestParam String amount, @RequestParam String name){
-        EmailService emailService = new EmailService();
-        emailService.email(amount, name);
+        emailService.emailStockNotification(amount, name);
     }
+
 }
--- src/main/java/com/hsleiden/vdlelie/services/EmailService.java
+++ src/main/java/com/hsleiden/vdlelie/services/EmailService.java
@@ -1,6 +1,5 @@
 package com.hsleiden.vdlelie.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -32,17 +31,22 @@ public class EmailService {
     }
 
 
-    public void email(String amount, String name){
-        sendEmail(bodyCreator(amount, name), "Notification: Stock running low", "r.j.colijn@hotmail.com");
+    public void emailStockNotification(String amount, String name){
+        sendEmail(bodyCreatorLowSock(amount, name), "Notification: Stock running low", "r.j.colijn@hotmail.com");
     }
 
-    String bodyCreator(String amount, String name){
+
+
+    String bodyCreatorLowSock(String amount, String name){
         String body = "Notification" + "\n" +
                 "" + "\n" +
                 "Stock " + name + " is running low on stock" + "\n" +
                 "There are only " + amount + " left in the current stock";
         return body;
     }
+
+
+
 
      void sendEmail(String body, String subject, String to){
         SimpleMailMessage message = new SimpleMailMessage();
